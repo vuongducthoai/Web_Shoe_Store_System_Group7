7a6c27ab6d3437e649cd2b63466c6a1bcc20450a
diamondbusiness <tungnd21896@gmail.com>
2024-11-07 23:23:29 +0700

    123

---
+++ src/main/java/entity/CartItem.java
@@ -0,0 +1,17 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CardItem {
+
+    private card
+}
---
+++ src/main/java/entity/Review.java
@@ -0,0 +1,21 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notify {
+
+    private int notifyID;
+    private Date date;
+    private String comment;
+}
