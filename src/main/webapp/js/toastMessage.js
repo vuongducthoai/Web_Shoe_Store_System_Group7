fe6990386c53e20ab116560097ad8d43264279fe
vuongducthoai <thoai12309@gmail.com>
2024-11-20 23:12:17 +0700

    123

--- src/main/webapp/js/toastMessage.js
+++ src/main/webapp/js/toastMessage.js
@@ -1,10 +1,9 @@
-function showSuccessToast() {
-    alert("123333");
+function showSuccessToast({ title, message, type }) {
     custom_toast({
-        title: "Welcome back!",
-        message: "We're so happy you're back! 😊 Let's make today amazing!",
-        type: "success",
-        duration: 2000
+        title: title,
+        message:message,
+        type: type,
+        duration: 6000
     });
 }
 
@@ -13,7 +12,7 @@ function showErrorToast() {
         title: "We are very sorry for not satisfying you",
         message: "Please accept my apologies for the lack of deliciousness in the food",
         type: "error",
-        duration: 1000
+        duration: 6000
     });
 }
 
@@ -23,7 +22,7 @@ function custom_toast({ title = "", message = "", type = "success", duration = 1
         const toast = document.createElement("div");
 
         // Auto remove toast
-        const autoRemoveId = setTimeout(() => main.removeChild(toast), duration + 300);
+        const autoRemoveId = setTimeout(() => main.removeChild(toast), duration + 1000);
 
         toast.onclick = (e) => {
             if (e.target.closest(".toast__close")) {
@@ -32,7 +31,6 @@ function custom_toast({ title = "", message = "", type = "success", duration = 1
             }
         };
 
-        alert("123");
 
         const icons = {
             success: "fas fa-check-circle",
@@ -43,7 +41,7 @@ function custom_toast({ title = "", message = "", type = "success", duration = 1
         const icon = icons[type];
         const delay = (duration / 1000).toFixed(2);
 
-        toast.classList.add("toast", `toast--${type}`);
+        toast.classList.add("toast_custom", `toast--${type}`);
         toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
 
         toast.innerHTML = `
