0f832ac8d58c3a0ac7201e2df5efecb6f482ad50
22110440 <157661962+22110440@users.noreply.github.com>
2024-11-24 11:24:31 +0700

    TRI

--- src/main/java/dto/CustomerDTO.java
+++ src/main/java/dto/CustomerDTO.java
@@ -14,7 +14,7 @@ import java.util.Date;
 @AllArgsConstructor
 public class CustomerDTO extends UserDTO {
 
-    private int customerId;
+    private int userId;
     private Date dateOfBirth;
     private int loyalty;
     private AddressDTO addressDTO;
