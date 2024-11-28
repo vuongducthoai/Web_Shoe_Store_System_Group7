2341bbe8fa1069a4b4d3536168f91b7e0917ec1d
xdung04 <157205064+xdung04@users.noreply.github.com>
2024-11-28 12:08:38 +0700

    Update ProductPromotionImpl.java

--- src/main/java/dao/Impl/ProductPromotionImpl.java
+++ src/main/java/dao/Impl/ProductPromotionImpl.java
@@ -14,6 +14,7 @@ import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class ProductPromotionImpl implements IProductPromotion {
     @Override
